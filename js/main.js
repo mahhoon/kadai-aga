a1b40ab8ef7ccd8173abf14be54e9bd988e4a771
Ubuntu <ubuntu@ip-172-31-11-207.ap-northeast-1.compute.internal>
2021-11-14 00:06:42 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,31 +1,25 @@
 
 
 $(function() {
-//クリニックph表示
-  $(window).scroll(function() {
-    $('.clinic-ph').each(function() {
-      const targetElement = $(this).offset().top;
-      const scroll = $(window).scrollTop();
-      const windowHeight = $(window).height();
+  //フェードイン
+  const fadeInPhoto = (element, view) => {
+    $(window).scroll(function() {
+      $(element).each(function() {
+        const targetElement = $(this).offset().top;
+        const scroll = $(window).scrollTop();
+        const windowHeight = $(window).height();
 　        
-      if (scroll > targetElement - windowHeight + 300) {
-        $(this).addClass('clinic-ph-view')
-      }
+        if (scroll > targetElement - windowHeight + 300) {
+          $(this).addClass(view)
+        }
+      });
     });
-  });
+  }
   
+  //クリニックph表示
+  fadeInPhoto('.clinic-ph', 'clinic-ph-view');
   //症例写真表示
-  $(window).scroll(function() {
-    $('.case-photo-wrap,.case-photo-wrap-sp').each(function() {
-      const targetElement = $(this).offset().top;
-      const scroll = $(window).scrollTop();
-      const windowHeight = $(window).height();
-      
-      if (scroll > targetElement - windowHeight + 300) {
-        $(this).addClass('case-photo-fadein')
-      }
-    });
-  });
+  fadeInPhoto('.case-photo-wrap,.case-photo-wrap-sp', 'case-photo-fadein');
   
   //アコーディオン
   $('.acordion-btn-blue, .acordion-btn-orange').on('click', (e) => {
